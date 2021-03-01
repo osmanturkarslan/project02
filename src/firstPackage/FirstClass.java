3c59c590acfd12775676ec29ec0246c61e4a7039
osmanturkarslan <79464729+osmanturkarslan@users.noreply.github.com>
2021-03-01 12:31:21 +0200

    forth commit from remote master branch

--- src/firstPackage/FirstClass.java
+++ src/firstPackage/FirstClass.java
@@ -9,6 +9,7 @@ public class FirstClass {
 		System.out.println("first commit from remote master branch");
 		System.out.println("second commit from remote master branch");
 		System.out.println("third commit from remote master branch");
+		System.out.println("Here is the conflict we have from remote master branch");
 	}
 
 }
