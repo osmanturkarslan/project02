c8b3651fc2e7c49663401184ba4536cc7e5a7842
Osman Turkarslan <osmanturkarslan1620@gmail.com>
2021-03-01 11:26:37 +0200

    second commit from local master

--- src/firstPackage/FirstClass.java
+++ src/firstPackage/FirstClass.java
@@ -4,7 +4,7 @@ public class FirstClass {
 
 	public static void main(String[] args) {
 		System.out.println("first commit from local master branch");
-
+		System.out.println("second commit from local master");
 	}
 
 }
